g7/python-nala

commit d97debe6962624fd795d988c6efcafbdc1397f2e

Queue: pass a list of the applications interested by the Queue when emitting the processable signal.

nala/queue.py
@@ -46,7 +46,7 @@ class Queue(GObject.GObject):
 	  """
 	
 	__gsignals__ = {
-		"processable" : (GObject.SIGNAL_RUN_FIRST, None, (GObject.TYPE_PYOBJECT,))
+		"processable" : (GObject.SIGNAL_RUN_FIRST, None, (GObject.TYPE_PYOBJECT, GObject.TYPE_PYOBJECT,))
 	}
 		
 	in_queue = []
@@ -78,10 +78,17 @@ class Queue(GObject.GObject):
 		the current Queue to the listeners of the 'processable' signal. """
 		
 		if not override:
-			self.emit("processable", self.in_queue)
-			self.in_queue = []
-		else:
-			self.emit("processable", override)
+			override = self.in_queue
+		
+		# Get application list to process
+		apps = []
+		for item in override:
+			if item[0] in self.triggers:
+				# item[0] is watcher's path
+				for app in self.triggers[item[0]]:
+					if not app in apps: apps.append(app)
+		
+		self.emit("processable", apps, self.in_queue)
 		
 		# Remove the timeout
 		if self.timeout:

nala-watcher.py
@@ -24,8 +24,8 @@ from nala.watchers import WatcherPool
 from nala.queue import Queue
 from nala.applications import Application
 
-def on_incoming_queue(self, lst):
-	print "GOT QUEUE!", lst
+def on_incoming_queue(self, apps, lst):
+	print "GOT QUEUE!", apps, lst
 
 def add_to_queue(pool, watcher, trigger, event, queue):
 	return queue.add_to_queue(watcher, trigger, event)

commit 63c0c66e907a930345e54f6f6f7005f78739c53d

Fixed regression introduced in d97debe6962624fd795d988c6efcafbdc1397f2e.
The in_queue list was not being cleared anymore.

nala/queue.py
@@ -79,7 +79,7 @@ class Queue(GObject.GObject):
 		
 		if not override:
 			override = self.in_queue
-		
+				
 		# Get application list to process
 		apps = []
 		for item in override:
@@ -89,6 +89,8 @@ class Queue(GObject.GObject):
 					if not app in apps: apps.append(app)
 		
 		self.emit("processable", apps, self.in_queue)
+		
+		self.in_queue = [] 
 		
 		# Remove the timeout
 		if self.timeout: